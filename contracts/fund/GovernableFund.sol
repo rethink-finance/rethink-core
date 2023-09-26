d19e4ff09932f296fb17c04b267c49bd2ccc644e
cinquemb <1519017+cinquemb@users.noreply.github.com>
2023-09-26 08:57:06 -0400

    working on nav storage types/logic

--- contracts/fund/GovernableFund.sol
+++ contracts/fund/GovernableFund.sol
@@ -14,6 +14,7 @@ contract GovernableFund is ERC20Votes {
 	uint256 _navUpdateLatestIndex;
 	uint256 _navUpdateLatestTime;
 	mapping(uint256 => uint256) navUpdatedTime;
+	mapping(uint256 => NavUpdateEntry[]) navUpdate;//nav update index -> nav entries for update
 	bool isRequestedWithdrawals;
 
 	Settings FundSettings;
@@ -30,20 +31,85 @@ contract GovernableFund is ERC20Votes {
 		uint256 withdrawFee;
 		uint256 performanceFee;
 		uint256 managementFee;
-
+		uint256 performaceHurdleRateBps;
 		address baseToken;
 		address safe; //TODO: needs to be set after safe creation
+		bool isExternalGovTokenInUse;
+		address governaceToken;
+	}
+
+	/*
+		TODO:
+		Manager will input:
+		Token pair address or aggregator address
+		Function signature to query floor price
+		Function # of inputs
+		List of inputs
+		Base currency token address
+		Asset token address				
+	*/
+
+	struct NAVLiquidUpdate {
+		address tokenPair;
+		address aggregatorAddress;
+		bytes functionSignatureWithEncodedInputes;
+		address assetTokenAddress;
+		address nonAssetTokenAddress;
+	}
+
+	/*
+		Address of acquired token
+		Amount of base currency used to acquire token
+		List of txs hashes used to acquire token
+		Amount of tokens acquired
+	*/
+	struct NAVIlliquidUpdate {
+		uint256 baseCurrencySpent;
+		uint256 amountAquiredTokens;
+		address tokenAddress;
+		string[] otcTxHashes;
+	}
+
+	/*
+		TODO
+		Chainlink https://docs.chain.link/data-feeds/nft-floor-price/addresses 
+
+	*/
+	struct NAVNFTUpdate {
+		address oracleAddress;
+		address nftAddress;
+	}
+
+	/*
+		TODO:
+		The address that implements some pricing function (can be found in a protocols documentation)
+		Function Signature (i.e “transferFrom(address,address,uint256)”, can be found from verified smart contracts in explorer, the open source code for a project)
+		Total amount of input values: (i.e “3”)
+		Specific input values for the Function to get the required position value (i.e. “0x123,0x456,789)
+		Decimals used to normalise position output (i.e “18”)
+
+	*/
+	struct NAVComposableUpdate {
+		address remoteContractAddress;
+		string functionSignatures;
+		bytes encodedFunctionSignaturWithInputs;
+		uint256 normalizationDecimals;
 	}
 
 	enum NavUpdateType {
-		NAVLiquidUpdate,
-		NAVIlliquidUpdate,
-		NAVNftUpdate,
-		NAVComposableUpdate
+		NAVLiquidUpdateType,
+		NAVIlliquidUpdateType,
+		NAVNFTUpdateType,
+		NAVComposableUpdateType
 	}
 
 	struct NavUpdateEntry {
+		uint256 updateTime;
 		NavUpdateType entryType;
+		NAVLiquidUpdate[] liquid;
+		NAVIlliquidUpdate[] illiquid;
+		NAVNFTUpdate[] nft;
+		NAVComposableUpdate[] composable;
 	}
 
 	struct WithdrawalRequestEntry {
@@ -52,6 +118,14 @@ contract GovernableFund is ERC20Votes {
 	}
 
 	function updateSettings() external {}
+
+	function updateNav(NavUpdateEntry[] calldata navUpdateData) external {
+		//TODO: can be triggered by governance or fund manager
+	}
+
+	function computeLatestNav() public view returns (uint) {}
+
+	function computeNavAtIndex(uint256 navUpdateIndex) public view returns (uint) {}
 
 	function deposit(uint256  amount) external {
 		//TODO: still need to implement
