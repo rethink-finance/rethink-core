74823d4ce7887f6a34d32df8463ab61fe84faed7
cinquemb <1519017+cinquemb@users.noreply.github.com>
2023-11-07 21:48:12 -0500

    fix: do not default to timelock addr for executor

--- migrations/13_deploy_gov_contract_factory_singleton.js
+++ migrations/13_deploy_gov_contract_factory_singleton.js
@@ -8,15 +8,14 @@ const execData = "0x";
 const proxy = "0xcd1e65B55cd73860FC80778C8398ae2f4C9222e8";
 //time truffle migrate --reset -f 13 --to 13 --skip-dry-run --network=goerli
 module.exports = async function (deployer) {
-	/*
 	let gff = await deployer.deploy(GovernableContractFactory);
 	setTimeout(function(){},delay);
 	console.log("GovernableContractFactory singleton is at: "+ GovernableContractFactory.address);
-	*/
+	
 	let ub = await UpgradeableBeacon.at(proxy);
 	setTimeout(function(){},delay);
-	ub.upgradeTo("0xd6d86a6B9207AdB6BFf1949aa440cdE23AdC1e77");
-	//ub.upgradeTo(GovernableContractFactory.address);
+	//ub.upgradeTo("0xd6d86a6B9207AdB6BFf1949aa440cdE23AdC1e77");
+	ub.upgradeTo(GovernableContractFactory.address);
 	
 	/*
   	let gffub = await deployer.deploy(UpgradeableBeacon, GovernableContractFactory.address);
