85ad4ff2a1ac79f04d44da35cd0371b846ff9cd6
cinquemb <1519017+cinquemb@users.noreply.github.com>
2023-12-04 21:00:54 -0500

    fix: reverse sign for checking if enough funds in fund contract for meeting redemptions

--- contracts/fund/GovernableFund.sol
+++ contracts/fund/GovernableFund.sol
@@ -84,7 +84,7 @@ contract GovernableFund is ERC20VotesUpgradeable, GovernableFundStorage {
 		//_nav = processNav(navUpdateData);
 
 		//NOTE: could be some logic to better handle deposit/withdrawal flows
-		require(((_nav + IERC20(FundSettings.baseToken).balanceOf(FundSettings.safe) - _depositBal) * _withdrawalBal / totalSupply()) <= IERC20(FundSettings.baseToken).balanceOf(address(this)), 'not enough for withdrawals');
+		require(((_nav + IERC20(FundSettings.baseToken).balanceOf(FundSettings.safe) - _depositBal) * _withdrawalBal / totalSupply()) >= IERC20(FundSettings.baseToken).balanceOf(address(this)), 'not enough for withdrawals');
 		isRequestedWithdrawals = false;
 		_navUpdateLatestIndex++;
 	}
