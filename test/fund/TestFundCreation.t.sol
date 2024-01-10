c9803aaaa7a73ed76965047a6de0c41387ee84e2
cinquemb <1519017+cinquemb@users.noreply.github.com>
2024-01-10 02:37:56 -0500

    first pass at testFundCreationDefault

--- test/fund/TestFundCreation.t.sol
+++ test/fund/TestFundCreation.t.sol
@@ -7,7 +7,30 @@ import "./Base.t.sol";
 
 contract TestFundCreation is Base {
 	
-	function testFundCreationDefault() public {}
+	struct GovernorParams {
+		uint256 quorumFraction;
+		uint256 lateQuorum;
+		uint256 votingDelay;
+		uint256 votingPeriod;
+		uint256 proposalThreshold;
+	}
+	
+	function testFundCreationDefault() public {
+		//function createFund(IGovernableFundStorage.Settings memory fundSettings, GovernorParams memory governorSettings, string memory _fundMetadata)
+
+		IGovernableFundStorage.Settings memory fundSettings;
+		GovernorParams memory governorSettings;
+		string memory _fundMetadata = "{}";
+
+		bytes memory gffCreateFund = abi.encodeWithSelector(
+            bytes4(keccak256("createFund(IGovernableFundStorage.Settings, GovernorParams, string)")),
+            fundSettings,
+            governorSettings,
+            _fundMetadata
+        );
+        (bool success,) = gffub.call(gffCreateFund);
+        require(success == true, "fail gff createFund");
+	}
 
 	function testFundCreationExternalERC20VotesCompatGov() public {}
 
