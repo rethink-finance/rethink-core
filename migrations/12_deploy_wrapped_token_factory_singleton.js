eaa5042b37f2fe7286bc7fec8f542404d8402dcd
cinquemb <1519017+cinquemb@users.noreply.github.com>
2024-01-25 20:48:11 -0500

    post audit changes redeploy

--- migrations/12_deploy_wrapped_token_factory_singleton.js
+++ migrations/12_deploy_wrapped_token_factory_singleton.js
@@ -12,21 +12,21 @@ const BeaconProxy = artifacts.require("BeaconProxy");
 const delay = 10000;
 const owner = "0xe977757dA5fd73Ca3D2bA6b7B544bdF42bb2CBf6";
 const execData = "0x";
-const proxy = "";
+const proxy = "0xB9Ca0051232F773Bd3C6A7823E02449783a2B53F"; //matic
 //time truffle migrate --reset -f 12 --to 12 --skip-dry-run --network=goerli
 module.exports = async function (deployer) {
-	let gff = await deployer.deploy(WrappedTokenFactory);
-	setTimeout(function(){},delay);
-	console.log("WrappedTokenFactory singleton is at: "+ WrappedTokenFactory.address);
+	//let gff = await deployer.deploy(WrappedTokenFactory);
+	//setTimeout(function(){},delay);
+	//console.log("WrappedTokenFactory singleton is at: "+ WrappedTokenFactory.address);
 
-	/*
 	let ub = await UpgradeableBeacon.at(proxy);
 	setTimeout(function(){},delay);
-	ub.upgradeTo(GovernableFundFactory.address);
+	//ub.upgradeTo(WrappedTokenFactory.address);
+	ub.upgradeTo("0x616FFa09771e270aeec04213aafE40309F5b0d6b");
 	
-	*/
+	/*
   	let gffub = await deployer.deploy(UpgradeableBeacon, WrappedTokenFactory.address);
   	setTimeout(function(){},delay);
   	console.log("WrappedTokenFactoryUpgradeableBeacon is at: "+ UpgradeableBeacon.address);
-  	/**/
+  	*/
 }
--- migrations/3_deploy_fund_singleton.js
+++ migrations/3_deploy_fund_singleton.js
@@ -7,8 +7,8 @@ const delay = 10000;
 const owner = "0xe977757dA5fd73Ca3D2bA6b7B544bdF42bb2CBf6";
 const execData = "0x";
 //const proxy = "0xCEed8bA2ea5B30eDf31a4c022F51FF0FE4d30166";//goerli
-const proxy = "0x5A7f717B91c998d5DE9764DEA78c2EF20027bDe4";//polygon
-//time truffle migrate --reset -f 3 --to 3 --skip-dry-run --compile-none --network=goerli
+const proxy = "0x5A7f717B91c998d5DE9764DEA78c2EF20027bDe4";//matic
+//time truffle migrate --reset -f 3 --to 3 --skip-dry-run --compile-none --network=matic
 module.exports = async function (deployer) {
 	/**/
 	let gf = await deployer.deploy(GovernableFund);
