e0f52fc5df838ade20b0bbf85cd0d157fd07a852
cinquemb <1519017+cinquemb@users.noreply.github.com>
2024-02-21 22:19:21 -0500

    script for beacon ownership transfer

---
+++ migrations/14_change_beacon_ownership.js
@@ -0,0 +1,64 @@
+const GovernableFundFactory = artifacts.require("GovernableFundFactory");
+const UpgradeableBeacon = artifacts.require("UpgradeableBeacon");
+
+//import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+const delay = 10000;
+const execData = "0x";
+const proxy = "";
+const isTest = false;
+const network = "matic";
+
+//time truffle migrate --reset -f 14 --to 14 --compile-none --skip-dry-run --network=arb1
+
+const multiSigs = {
+	"arb1": "0xd8a5076Da13a2d95062de42dD26CB41B9Daa0B53",
+	"matic": "0x83f2492DB796BF0692C90B65DeBB926bd688fAe5"
+};
+
+const proxyTypes = [
+	"GovernableFundFactoryUpgradeableBeacon",
+	"RethinkFundGovernerUpgradeableBeacon",
+	"GovernableFundUpgradeableBeacon",
+	"WrappedTokenFactoryUpgradeableBeacon",
+	"NAVCalculatorUpgradeableBeacon",
+	"ZodiacRolesV1ModifierUpgradeableBeacon",
+	"GovernableFundFlowsUpgradeableBeacon",
+	"GovernableFundNavUpgradeableBeacon",
+	"GovernableContractFactoryUpgradeableBeacon"
+];
+
+const proxies = {
+	"arb1": {
+		"GovernableFundFactoryUpgradeableBeacon": "0xB9Ca0051232F773Bd3C6A7823E02449783a2B53F",
+		"RethinkFundGovernerUpgradeableBeacon": "0x248a64e3EDd3F521ef2Aa6A3e804845B5A1C8008",
+		"GovernableFundUpgradeableBeacon": "0xB4c232f0cF194E530c39174F617Ec4ee9d69398C",
+		"WrappedTokenFactoryUpgradeableBeacon": "0x4278a6b150628470F28Af2Df6B43518f372A59E4",
+		"NAVCalculatorUpgradeableBeacon": "0x4C342E583A7Aa2840e07B4a3afB71533FBE37726",
+		"ZodiacRolesV1ModifierUpgradeableBeacon": "0x5A7f717B91c998d5DE9764DEA78c2EF20027bDe4",
+		"GovernableFundFlowsUpgradeableBeacon": "0xdf587D859e76B0a6cE2254f1c0bf64C4aE0eD37f",
+		"GovernableFundNavUpgradeableBeacon": "0x8fE2e9470ceA2E83e8B89502d636CCAb2D1Ca21B",
+		"GovernableContractFactoryUpgradeableBeacon": "0x89254d6FF377a21aC0b99BD2e456e75b6C76E505"
+	},
+	"matic": {
+		"GovernableFundFactoryUpgradeableBeacon": "0xE16b6C9C2CB8aE15f0872A3A46d2Eb070c27f20D",
+		"RethinkFundGovernerUpgradeableBeacon": "0xB4c232f0cF194E530c39174F617Ec4ee9d69398C",
+		"GovernableFundUpgradeableBeacon": "0x5A7f717B91c998d5DE9764DEA78c2EF20027bDe4",
+		"WrappedTokenFactoryUpgradeableBeacon": "0xB9Ca0051232F773Bd3C6A7823E02449783a2B53F",
+		"NAVCalculatorUpgradeableBeacon": "0xe0D8ca6d6b67E39dfADC137B675455Bc1ee5bCdd",
+		"ZodiacRolesV1ModifierUpgradeableBeacon": "0xdf587D859e76B0a6cE2254f1c0bf64C4aE0eD37f",
+		"GovernableFundFlowsUpgradeableBeacon": "0x8fE2e9470ceA2E83e8B89502d636CCAb2D1Ca21B",
+		"GovernableFundNavUpgradeableBeacon": "0x89254d6FF377a21aC0b99BD2e456e75b6C76E505",
+		"GovernableContractFactoryUpgradeableBeacon": "0x89483Dc199F70268e3aB79D08301456Fb6aF75f4"
+	}
+};
+module.exports = async function (deployer) {
+	for (var i=0; i<proxyTypes.length;i++) {
+		let ub = await UpgradeableBeacon.at(proxies[network][proxyTypes[i]]);
+		setTimeout(function(){},delay);
+		console.log(proxyTypes[i] +" at " + proxies[network][proxyTypes[i]] + " ownership has transfered to: "+ multiSigs[network]);
+		//ub.transferOwnership(multiSigs[network]);
+		//setTimeout(function(){},delay);
+	}
+	throw '';
+}
