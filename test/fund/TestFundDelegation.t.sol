a08fc02d9f29823faf8eee87a0f28396d03ac3bf
cinquemb <1519017+cinquemb@users.noreply.github.com>
2024-02-23 04:02:09 -0500

    unit tests: working on getting fund addrs after fund creation

--- test/fund/TestFundDelegation.t.sol
+++ test/fund/TestFundDelegation.t.sol
@@ -5,23 +5,31 @@ import "truffle/Assert.sol";
 import "../common/utils/MoreAssert.t.sol";
 import "./Base.t.sol";
 import "../../contracts/interfaces/fund/IGovernableFund.sol";
+import "../../contracts/interfaces/fund/IGovernableFundFactory.sol";
 import "../common/Agent.t.sol";
 import "forge-std/console.sol";
 
 contract TestFundDelegation is Base {
 	function testFundDelegationAfterDeposit() public {
-		address[] memory allowedDepositAddrs;
-		bytes memory gffCreateFund = this.createFund(address(this), allowedDepositAddrs, address(0));
-        (bool success, bytes memory data) = gff.call(gffCreateFund);
-        require(success == true, "fail createFund");
+	     initFund();
+	     IGovernableFundFactory(gff).registeredFunds(0, 1);
 
-        console.logBytes(data);//TODO: note this is not addres but some kind of code for a contract that is returned here
-        address fundAddr = abi.decode(data, (address));
+        //address fundAddr = abi.decode(data, (address));
 
         //IGovernableFundStorage.Settings memory settings = IGovernableFund(fundAddr).getFundSettings();
         //Agent bob = new Agent();
         //bob.requestDeposit(settings.baseToken, fundAddr, 10e18);
         //bob.deposit(fundAddr);
         //bob.delegate(fundAddr, address(bob));
+	}
+
+	function initFund() private {
+		address[] memory allowedDepositAddrs;
+		bytes memory gffCreateFund = this.createFund(address(this), allowedDepositAddrs, address(0));
+
+		(bool success, /*bytes memory data0*/) = gff.call(gffCreateFund);
+        require(success == true, "fail createFund");
+
+        //console.logBytes(data0);//TODO: note this is not addres but some kind of code for a contract that is returned here
 	}
 }
